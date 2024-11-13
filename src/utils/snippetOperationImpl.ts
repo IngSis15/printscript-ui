58450e2d3859bc1792dc58ae1ed8a59b2aa2ae77
ManuLosta <mlostalom@gmail.com>
2024-11-13 19:29:33 -0300

    some snippet operations

--- src/utils/snippetOperationImpl.ts
+++ src/utils/snippetOperationImpl.ts
@@ -3,7 +3,7 @@ import {CreateSnippet, PaginatedSnippets, Snippet, UpdateSnippet} from "./snippe
 import {FileType} from "../types/FileType.ts";
 import {Rule} from "../types/Rule.ts";
 import {TestCase} from "../types/TestCase.ts";
-import {PaginatedUsers} from "./users.ts";
+import {PaginatedUsers, User} from "./users.ts";
 import {TestCaseResult} from "./queries.tsx";
 import {axiosInstance} from "./axios.config.ts";
 
@@ -15,13 +15,15 @@ export class SnippetOperationImpl implements SnippetOperations {
       version: "1.1"
     }
 
-    return axiosInstance.post("/snippet/v1/snippet", {
-      body
-    })
+    try {
+      return axiosInstance.post("/snippet/v1/snippet", body)
+    } catch (e) {
+      throw new Error("Snippet creation failed");
+    }
   }
 
   deleteSnippet(id: string): Promise<string> {
-    return Promise.resolve("");
+    return axiosInstance.delete(`/snippet/v1/snippet/${id}`)
   }
 
   formatSnippet(snippet: string): Promise<string> {
@@ -37,36 +39,95 @@ export class SnippetOperationImpl implements SnippetOperations {
     ])
   }
 
-  getFormatRules(): Promise<Rule[]> {
-    return Promise.resolve([]);
+  async getFormatRules(): Promise<Rule[]> {
+    try {
+      const response = await axiosInstance.get("/snippet/v1/config/formatting")
+      const data = response.data
+      return this.jsonToRules(data)
+    } catch (e) {
+      throw new Error("Fetching format rules failed")
+    }
   }
 
-  getLintingRules(): Promise<Rule[]> {
-    return Promise.resolve([]);
+  async getLintingRules(): Promise<Rule[]> {
+    try {
+      const response = await axiosInstance.get("/snippet/v1/config/linting")
+      const data = response.data
+      return this.jsonToRules(data)
+    } catch (e) {
+      throw new Error("Fetching linting rules failed");
+    }
   }
 
-  getSnippetById(id: string): Promise<Snippet | undefined> {
-    return Promise.resolve(undefined);
+  async getSnippetById(id: string): Promise<Snippet | undefined> {
+    try {
+      const response = await axiosInstance.get(`/snippet/v1/snippet/${id}`)
+      return response.data
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   getTestCases(): Promise<TestCase[]> {
     return Promise.resolve([]);
   }
 
-  getUserFriends(name?: string, page?: number, pageSize?: number): Promise<PaginatedUsers> {
-    return Promise.resolve(undefined);
+  async getUserFriends(_name?: string = "", _page?: number, _pageSize?: number): Promise<PaginatedUsers> {
+    try {
+      const response = await axiosInstance.get("/permission/users")
+      const data = response.data
+
+      return {
+        page: 1,
+        page_size: 99,
+        count: data.length,
+        users: data.map((user: any): User => ({
+          name: user.name,
+          id: user.user_id,
+        }))
+      }
+    } catch (e) {
+      throw new Error("Fetching user friends failed");
+    }
   }
 
-  listSnippetDescriptors(page: number, pageSize: number, sippetName?: string): Promise<PaginatedSnippets> {
-    return Promise.resolve(undefined);
+  async listSnippetDescriptors(page: number, pageSize: number, _snippetName?: string): Promise<PaginatedSnippets> {
+    try {
+      const response = await axiosInstance.get("/snippet/v1/snippet", {
+        params: {
+          page,
+          size: pageSize,
+        }
+      })
+
+      const data = response.data;
+      return {
+        snippets: data.content,
+        page: data.pageable.pageNumber + 1, // Adjusting for 1-based page index if needed
+        page_size: data.pageable.pageSize,
+        count: data.totalElements
+      };
+    } catch (e) {
+      throw new Error("Failed to fetch snippet descriptors");
+    }
   }
 
-  modifyFormatRule(newRules: Rule[]): Promise<Rule[]> {
-    return Promise.resolve([]);
+  async modifyFormatRule(newRules: Rule[]): Promise<Rule[]> {
+    try {
+      const response = await axiosInstance.put("/snippet/v1/config/formatting", this.rulesToJson(newRules))
+      return this.jsonToRules(response.data)
+    } catch (e) {
+      throw new Error("Failed modifying format rules")
+    }
   }
 
-  modifyLintingRule(newRules: Rule[]): Promise<Rule[]> {
-    return Promise.resolve([]);
+  async modifyLintingRule(newRules: Rule[]): Promise<Rule[]> {
+    try {
+      const response = await axiosInstance.put("/snippet/v1/config/linting", this.rulesToJson(newRules))
+      return this.jsonToRules(response.data)
+    } catch (e) {
+      throw new Error("Failed modifying linting rules")
+    }
   }
 
   postTestCase(testCase: Partial<TestCase>): Promise<TestCase> {
@@ -85,7 +146,41 @@ export class SnippetOperationImpl implements SnippetOperations {
     return Promise.resolve(undefined);
   }
 
-  updateSnippetById(id: string, updateSnippet: UpdateSnippet): Promise<Snippet> {
-    return Promise.resolve(undefined);
+  async updateSnippetById(id: string, updateSnippet: UpdateSnippet): Promise<Snippet> {
+    const body = {
+      ...updateSnippet,
+      description: "",
+      version: "1.1"
+    }
+
+    try {
+      const response = await axiosInstance.post(`/snippet/v1/snippet/${id}`, body)
+      return response.data as Snippet
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+ rulesToJson(rules: Rule[]): { [key: string]: string | number | boolean } {
+   return rules.reduce((acc: { [key: string]: string | number | boolean }, rule) => {
+      if (rule.isActive && rule.value !== null && rule.value !== undefined) {
+        acc[rule.name] = rule.value;
+      } else if (rule.value === null || rule.value === undefined) {
+        acc[rule.name] = rule.isActive;
+      }
+      return acc;
+    }, {} as { [key: string]: string | number | boolean });
+  }
+
+  jsonToRules(json: { [key: string]: string | number | boolean }): Rule[] {
+    return Object.keys(json).map((key, index) => {
+      const value = json[key]
+      return {
+        id: index.toString(),
+        name: key,
+        isActive: typeof value === 'boolean' ? value : true,
+        value: typeof value !== 'boolean' ? value : null
+      }
+    })
   }
 }
