32db824823d84c1b538a4d212943e5fe2eeb80bd
ManuLosta <mlostalom@gmail.com>
2024-11-19 00:38:25 -0300

    implement format

--- src/utils/queries.tsx
+++ src/utils/queries.tsx
@@ -150,7 +150,7 @@ export const useFormatSnippet = () => {
   const snippetOperations = useSnippetsOperations()
 
   return useMutation<string, Error, string>(
-      snippetContent => snippetOperations.formatSnippet(snippetContent)
+      snippetId => snippetOperations.formatSnippet(snippetId)
   );
 }
 
